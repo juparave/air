c14051237144753ead87f68aa3e2e73812072d8f
Juan Pablo Ramírez <juparave@deuxbits.com>
2021-11-25 12:49:46 -0600

    Update util_linux.go

--- runner/util_linux.go
+++ runner/util_linux.go
@@ -29,6 +29,8 @@ func (e *Engine) killCmd(cmd *exec.Cmd) (pid int, err error) {
 
 func (e *Engine) startCmd(cmd string) (*exec.Cmd, io.WriteCloser, io.ReadCloser, io.ReadCloser, error) {
 	c := exec.Command("/bin/sh", "-c", cmd)
+	// assign the child process a new PGID identical to its PID
+	c.SysProcAttr = &syscall.SysProcAttr{Setpgid: true}
 	stderr, err := c.StderrPipe()
 	if err != nil {
 		return nil, nil, nil, nil, err
